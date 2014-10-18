f224b276e3b49bbea6719265712a652b53f93c8b
tsbgroup <the@tsbgroup.ru>
2014-10-19 04:03:24 +1100

    Восстановление пересчета рейтинга

--- app/controllers/RateController.php
+++ app/controllers/RateController.php
@@ -47,4 +47,25 @@ class RateController extends BaseController
 
         return round(10*(($plusPerc+$coef*$coef/(2*$count)-$coef*sqrt(($plusPerc*(1-$plusPerc)+$coef*$coef/(4*$count))/$count))/(1+$coef*$coef/$count)), 2);
     }
+
+    protected function updateRate($modelId, $instanceId)
+    {
+        $rateValues  = Rate::whereRaw('module_id = ? && instance_id = ?', array($modelId, $instanceId))
+            ->groupBy('plus')
+            ->orderBy('plus', 'asc')
+            ->get(array('plus',DB::raw('count(*) as cnt')));
+
+
+        if(count($rateValues) == 0)
+            return 0;
+
+        if(count($rateValues) == 1)
+            return ($rateValues['plus'] == 0)?self::getRate(1, $rateValues['cnt']):self::getRate($rateValues['cnt'], 1);
+
+        $modelName = Models::find($modelId)->modelName;
+
+        $instance = $modelName::find($instanceId);
+        $instance->rate = self::getRate($rateValues[1]['cnt'], $rateValues[0]['cnt']);
+        $instance->save();
+    }
 } 
